2bb642b1ace7648d58011ae0047843d6c1ae2b72
Rashed Khan <abir97bd@gmail.com>
2020-09-07 15:17:09 +0600

    Updates

---
+++ precache-manifest.00ad135df4759e24e477c6d516f01b12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617090b927d9636498eb894f3317066d",
+    "url": "/image-compressor/index.html"
+  },
+  {
+    "revision": "3128841c07539a3ef2df",
+    "url": "/image-compressor/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b10d6f08aa42e7c50539",
+    "url": "/image-compressor/static/css/main.4689fc5a.chunk.css"
+  },
+  {
+    "revision": "3128841c07539a3ef2df",
+    "url": "/image-compressor/static/js/2.7084d180.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/image-compressor/static/js/2.7084d180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10d6f08aa42e7c50539",
+    "url": "/image-compressor/static/js/main.d0f85f7c.chunk.js"
+  },
+  {
+    "revision": "394ffeec397e9e4c1ede",
+    "url": "/image-compressor/static/js/runtime-main.285707cd.js"
+  }
+]);
